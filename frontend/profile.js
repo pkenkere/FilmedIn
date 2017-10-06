5ee43d4928daef927abc0e10884ce8d800759387
Bhavana Kakubal <bhavanak@Bhavanas-MacBook-Pro.local>
2017-10-06 01:54:58 -0400

    allowed user to edit major, year, and interests in profile page

--- frontend/profile.js
+++ frontend/profile.js
@@ -3,6 +3,10 @@ $( document ).ready(function() {
         $(this).on('keyup', function() {
             var major = $('#userMajor').val() == '' ? '---' : $('#userMajor').val();
             $('#result').html("Major: " + major);
+            var year = $('#userYear').val() == '' ? '---' : $('#userYear').val();
+            $('#resultY').html("Year: " + year);
+            var interest = $('#userInterests').val() == '' ? '---' : $('#userInterests').val();
+            $('#resultI').html("Interests: " + interest);
             /*document.getElementById("result").innerHTML="Major: " + major;
             var userVer = $('#result').innerHTML;
             localStorage.userMajorEdit = userVer;*/
