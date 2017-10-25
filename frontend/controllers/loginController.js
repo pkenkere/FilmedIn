29b1515b05bcacfb0a531f41cb1a37b428ffe0db
shreyashankar <shankar5>
2017-10-25 14:14:12 -0400

    Login connection

--- frontend/controllers/loginController.js
+++ frontend/controllers/loginController.js
@@ -1,26 +1,30 @@
 "use strict";
 var url = "localhost:5000";
-var loginCredentials = {
-    method: "POST",
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-        user: loginEmail.value,
-        pass: passLogin.value
-    })
+function loginPost(email, password) {
+  var loginCredentials = {
+      method: "POST",
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+          user: email,
+          pass: password
+      })
+  }
+  fetch(url+"/", loginCredentials)
+  .then(function(res){
+      if(res.ok){
+          //login to profile
+          location.href = "profile.html?user="+email;
+          console.log("login success!");
+      }
+      else{
+          //alert incorrect
+          incorrectCredentials();
+          console.log("incorrect username/password");
+      }
+  })
+  .catch(function(err){
+      console.log("POST request failed", err);
+  });
 }
-fetch(url+"/", loginCredentials)
-.then(function(res){
-    if(res.ok){
-        //login to profile
-        console.log("login success!");
-    }
-    else{
-        //alert incorrect
-        console.log("incorrect username/password");
-    }
-})
-.catch(function(err){
-    console.log("POST request failed", err);
-});
---
+++ frontend/index.js
@@ -0,0 +1,65 @@
+
+function body_onload() {
+  //accounts = new Array();
+  //getAccounts();
+  signUp.onclick = signUp_onclick;
+  loginButton.onclick = login_onclick;
+}
+
+function signUp_onclick() {
+  // var pass = document.getElementById('pass').value;
+  // var rePass = document.getElementById('rePass').value;
+  //
+  // if (pass !== rePass) {
+  //   alert("Passwords don't match");
+  //   return;
+  // }
+  // var email = document.getElementById('email').value;
+  // var firstname = document.getElementById('firstname').value;
+  // var lastname = document.getElementById('lastname').value;
+  // if (email == null || firstname == null || lastname == null) {
+  //   alert("Please enter all information");
+  //   return;
+  // }
+  // if (email == "" || firstname == "" || lastname == "") {
+  //   alert("Please enter all information");
+  //   return;
+  // }
+  // var account = {
+  //   Email : email,
+  //   Password : pass,
+  //   FirstName : firstname,
+  //   LastName : lastname
+  // };
+  //
+  // accounts.push(account);
+  // saveAccounts();
+  //
+  // localStorage.setItem("loggedInId", accounts.length - 1);
+  // location.href = "permInfo.html";
+}
+
+function login_onclick() {
+    loginPost(loginEmail.value, passLogin.value);
+  // var email = loginEmail.value;
+  // var pass = passLogin.value;
+  //
+  // for (var i = 0; i < accounts.length; i++) {
+  //   var account = accounts[i];
+  //   if (account.Email == email) {
+  //     if (account.Password !== pass) {
+  //       alert("Incorrect Password");
+  //       return;
+  //     }
+  //     else {
+  //       localStorage.setItem("loggedInId", i);
+  //       location.href = "profile.html";
+  //     }
+  //   }
+  // }
+}
+
+function incorrectCredentials() {
+  errorMsg.style.display = "block";
+  strongErrorAlert.style.display = "block";
+}
