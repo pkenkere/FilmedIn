f18bd09408298c6e440f6b2eb7936c1ce58c23b1
shreyashankar <shankar5>
2017-11-05 12:37:42 -0500

    Controller for admin corrected

--- frontend/JS/admin.js
+++ frontend/JS/admin.js
@@ -32,7 +32,9 @@ function displaySuccess() {
 
 function addEquipment() {
   var category = document.querySelector('input[name="categoryname"]:checked').value;
+  console.log(category);
   var availability = document.querySelector('input[name="yesorno"]:checked').value;
+  console.log(availability);
     addEquipmentPost(equipmentName.value, category, availability);
 }
 
