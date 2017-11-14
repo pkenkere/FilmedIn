09a2b5fc04b693615a6436f560fdc237631a6392
pkenkere <pkenkere@purdue.edu>
2017-11-13 22:57:30 -0500

    searchjobs controller

--- frontend/JS/searchJobs.js
+++ frontend/JS/searchJobs.js
@@ -5,6 +5,7 @@ function body_onload() {
   PostJobBtn.onclick = PostJobBtn_onclick;
   LogoutBtn.onclick = logout_onclick;
   RentBtn.onclick = rentBtn_onclick;
+  btnSrch.onclick = search;
 }
 
 function HomeBtn_onclick(){
@@ -30,4 +31,23 @@ function rentBtn_onclick(){
 function logout_onclick() {
   localStorage.removeItem("loggedInId");
   location.href = "../HTML/index.html";
+}
+
+function search(){
+  console.log(gender);
+  console.log(RoleTypes.value);
+  console.log(ageFrom.value);
+  console.log(ageTo.value);
+  console.log(ProductionTypes.value);
+  console.log(ethnicity.value);
+  var q = {
+    //gender:,
+    roleType: RoleTypes.value,
+    minAge: ageFrom.value,
+    maxAge: ageTo.value,
+    type: ProductionTypes.value,
+    ethnicity: ethnicity.value
+  }
+  var obj = JSON.stringify(q);
+
 }
