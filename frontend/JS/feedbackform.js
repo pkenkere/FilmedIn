1142020e1f94c7fe147b93099c4e66bc71969767
shreyashankar <shankar5>
2017-11-27 23:26:58 -0500

    JS file developed

---
+++ frontend/JS/feedbackform.js
@@ -0,0 +1,10 @@
+function body_onload() {
+  YESBtn.onclick = yes_onclick;
+  // PARTIALLYBtn.onclick = partially_onclick;
+  // NOBtn.onlick = no_onclick;
+}
+
+function yes_onclick() {
+  console.log("clicked");
+  document.getElementsByClassName("tickmark1").style.display="block";
+}
