3d93000ed5413cf90d74e824772a46631a848f62
Manu3397 <aggarw32@purdue.edu>
2017-11-27 23:09:30 -0500

    Added email dispatchers for equipment cancellation and reportsending

--- backend/modules/email-dispatcher.js
+++ backend/modules/email-dispatcher.js
@@ -68,6 +68,22 @@ EM.dispatchReport = function (account, callback) {
             }, callback );
 }
 
+EM.dispatchEquipmentCancellation = function (account, callback) {
+  var server = require("emailjs").server.connect(
+          {
+              host        : process.env.EMAIL_HOST || 'smtp.gmail.com',
+              user        : process.env.EMAIL_USER || 'feedback.filmedin@gmail.com',
+              password    : process.env.EMAIL_PASS || 'weshouldallknowit',
+              ssl         : true,
+          });
+  server.send({
+              from         : process.env.EMAIL_FROM || 'feedback.filmedin@gmail.com',
+              to           : 'feedback.filmedin@gmail.com',
+              subject      : 'Annoucement reported: '+account.title,
+              text         :  EM.composeCancelEquipment(account)
+            }, callback );
+}
+
 EM.composeFeedBack = function (acc) {
   var text = "You have received feedback from " + acc.email + ":\n\n";
   text += "Feedback: '" + acc.feedback + "'.\n";
@@ -95,8 +111,21 @@ EM.composeEquipmentEmail = function(o)
   return text;
 }
 
-EM.composeReport = function (o) {
+EM.composeReport = function (acc) {
   var text = "You have received a report from " + acc.email + ":\n\n";
-  text += "Report: '" + acc.report + "'.\n";
+  text += "The annoucement reported is " + acc.headline + ":\n\n";
+  text += acc.description;
+  text += "\n\nReport:\n'" + acc.report + "'.\n";
+  return text;
+}
+
+EM.composeCancelEquipment = function(o)
+{
+  var text = "The following user has asked for cancellation of the following equipment:\n\nUser: " +
+              o.user + "\n\nEquipment checkout cancelled:\n\n";
+  for (var i = 0; i < o.size; i++) {
+
+    text+=o.equipments[i].name + " ==> Category: " + o.equipments[i].category + "\n";
+  }
   return text;
 }
