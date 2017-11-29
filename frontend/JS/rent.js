05e9c95776b69f8dfd047349595fa2526f8782f5
Bhavana Kakubal <bhavanak@pal-nat186-53-67.itap.purdue.edu>
2017-11-28 19:34:27 -0500

    added date picker for rent equipment

--- frontend/JS/rent.js
+++ frontend/JS/rent.js
@@ -19,7 +19,7 @@ function disable() {
 var arr = new Array();
 function verify(divName) {
   disable();
-  
+
   var node = document.getElementById(divName);
   while (node.firstChild) {
     node.removeChild(node.firstChild);
@@ -30,6 +30,11 @@ function verify(divName) {
   //   inputElements[i].checked = false;
   // }
   arr.length = 0;
+  // var date = document.getElementById('dateToRent');
+  // node = document.createElement('LI');
+  // textnode = document.createTextNode('DATE: ' + (date.value));
+  // node.appendChild(textnode);
+  // document.getElementById(divName).appendChild(node);
   for(var i=0; i<inputElements.length; i++){
       if(inputElements[i].checked == true){
         node = document.createElement('LI');
