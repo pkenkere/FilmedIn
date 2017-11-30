b4d4f10cc43ddc86163f48264d1569032e886bdf
Bhavana Kakubal <bhavanak@Bhavanas-MacBook-Pro.local>
2017-11-29 22:16:40 -0500

    adding success box to job post

--- frontend/controllers/jobpostcontroller.js
+++ frontend/controllers/jobpostcontroller.js
@@ -40,3 +40,42 @@ function jobPost(email, title, prodtype, prodDescrip, DateAndLoc, expDate, isPai
       console.log("POST request failed", err);
   });
 }
+
+function getAllJobs(){
+    var job = {
+        method : "GET"
+    }
+    fetch(url+"/jobs", job)
+    .then(function(res){
+      if(res.ok) {
+        res.json().then(function(data){
+          for (var i = 0; i < data.length; i++) {
+            var equip = data[i];
+            var label= document.createElement("label");
+            var description = document.createTextNode(equip.name);
+            var checkbox = document.createElement("input");
+
+            checkbox.type = "checkbox";    // make the element a checkbox
+            checkbox.id = i;      // give it id
+            checkbox.value = equip.name;         // make its value
+
+            label.appendChild(checkbox);   // add the box to the element
+            label.appendChild(description);// add the description to the element
+
+            // add the label element to your div
+            document.getElementById(equip.category).appendChild(label);
+          }
+            //console.log(data);
+            //var json = JSON.parse(data);
+            //console.log(json);
+            //console.log(data[0].name);
+        })
+      }
+      else {
+        console.log("incorrect username/password");
+      }
+    })
+    .catch(function(err){
+        console.log("GET request failed", err);
+    });
+}
