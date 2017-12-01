1ad65d8c34b4c70f4ec3302698124de7e5bba0bd
Bhavana Kakubal <bhavanak@Bhavanas-MacBook-Pro.local>
2017-11-30 22:31:27 -0500

    applicants show up for the user's posted job and is directed to their profile page

--- frontend/JS/profile.js
+++ frontend/JS/profile.js
@@ -2,7 +2,7 @@ var count = 0;
 
 function body_onload() {
   renderProfile();
-  //renderEquipments();
+  renderEquipments();
   getAllJobs();
   HomeBtn.onclick = HomeBtn_onclick;
   JobsBtn.onclick = JobsBtn_onclick;
