559df6c2d072b0d62edcf81d7fdc0044f43ea562
pkenkere <pkenkere@purdue.edu>
2017-12-03 01:30:16 -0500

    removed admin page links

--- frontend/JS/admin.js
+++ frontend/JS/admin.js
@@ -1,19 +1,19 @@
 var edata;
 
-function body_onload() {
+function body_onload() { 
   addEquip.onclick = addEquipment;
   addAdminBtn.onclick = addAdmin;
   addNewsBtn.onclick = addNews;
   ok.onclick = updateColor;
   okay.onclick = redirect;
-  HomeBtn.onclick = HomeBtn_onclick;
-  JobsBtn.onclick = JobsBtn_onclick;
-  ProfBtn.onclick = ProfBtn_onclick;
-  PostJobBtn.onclick = PostJobBtn_onclick;
+  // HomeBtn.onclick = HomeBtn_onclick;
+  // JobsBtn.onclick = JobsBtn_onclick;
+  // ProfBtn.onclick = ProfBtn_onclick;
+  // PostJobBtn.onclick = PostJobBtn_onclick;  
   LogoutBtn.onclick = logout_onclick;
-  RentBtn.onclick = rentBtn_onclick;
-  Profile.onclick = Profile_onclick;
-  FeedbackBtn.onclick = feedbackBtn_onclick;
+  // RentBtn.onclick = rentBtn_onclick;
+  // Profile.onclick = Profile_onclick;
+  // FeedbackBtn.onclick = feedbackBtn_onclick;
   displayFeedbacks();
   displayUsers();
   //removeEq.onclick = removeEquipment;
@@ -81,35 +81,35 @@ function redirect() {
   location.href = "../HTML/404notfound.html";
 }
 
-function HomeBtn_onclick(){
-  location.href = "../HTML/announcements.html";
-  //location.href = "../HTML/newsfeed.html";
-}
+// function HomeBtn_onclick(){
+//   location.href = "../HTML/announcements.html";
+//   //location.href = "../HTML/newsfeed.html";
+// }
 
-function JobsBtn_onclick(){
-  location.href = "../HTML/searchJobs.html";
-}
+// function JobsBtn_onclick(){
+//   location.href = "../HTML/searchJobs.html";
+// }
 
-function ProfBtn_onclick(){
-  location.href = "../HTML/findTalent.html";
-}
+// function ProfBtn_onclick(){
+//   location.href = "../HTML/findTalent.html";
+// }
 
-function PostJobBtn_onclick(){
-  location.href = "../HTML/Jobpost.html";
-}
+// function PostJobBtn_onclick(){
+//   location.href = "../HTML/Jobpost.html";
+// }
 
-function rentBtn_onclick(){
-  location.href = "../HTML/rentequip.html";
-}
+// function rentBtn_onclick(){
+//   location.href = "../HTML/rentequip.html";
+// }
 
 function logout_onclick() {
   localStorage.removeItem("loggedInId");
   location.href = "../HTML/index.html";
 }
 
-function Profile_onclick(){
-  location.href = "../HTML/profile.html"; //profile button takes you back to the admin page if you are an admin???
-}
+// function Profile_onclick(){
+//   location.href = "../HTML/profile.html"; //profile button takes you back to the admin page if you are an admin???
+// }
 
 function fillDiv(a) {
   //document.getElementById('All').innerHTML = "hbbhbibububybvgva";
